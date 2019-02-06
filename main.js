56a2371b456cec3657948799106b66aac8bb7570
Alona <alonavasylenko1996@gmail.com>
2019-02-06 16:53:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+$( "div.wrap" ).scrollTop( 300 );
+
+    $('.my-third-class').slick({
+        centerMode: true,
+        dots: true,
+        centerPadding: '60px',
+        slidesToShow: 5,
+        asNavFor: ".my-first-class"           
+    });
+
+    $('.my-first-class').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        fade: true,
+        asNavFor: ".my-second-class"   
+      });
+      $('.my-second-class').slick({
+        centerMode: true,
+        infinite: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        asNavFor: ".my-third-class"
+      });
+      
